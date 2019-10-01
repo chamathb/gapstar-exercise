a7dfbc5fd5b38ded7382dd1a0d07fba22a850cfb
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-10-01 13:35:24 +0530

    completing 2nd task, the fizzbuzz

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+/**
+ * @description prints 1 to `numberOfTimes` with use of randomWordSync
+ * @param numberOfTimes - accepts positive integer
+ */
 const printNumbersWithRandomWords = (numberOfTimes) => {
   if(!isNaN(numberOfTimes) && numberOfTimes > 0){
-    for(var i=0; i <= numberOfTimes; i++) {
-      let word = getRandomWordSync({ withErrors: false });
+    for(let i = 1; i <= numberOfTimes; i++) {
+      const word = getRandomWordSync({ withErrors: false });
       console.log(`${i}: ${word}`)
     }
   } else {
@@ -14,4 +18,22 @@ const printNumbersWithRandomWords = (numberOfTimes) => {
   }
 };
 
-printNumbersWithRandomWords(100);
+/**
+ * @description prints 1 to `numberOfTimes` with use of randomWordSync and FizzBuzz
+ * @param numberOfTimes - accepts positive integer
+ */
+const printNumbersWordsAndFizzBuzz = (numberOfTimes) => {
+  if(!isNaN(numberOfTimes) && numberOfTimes > 0){
+    for(let i=1; i <= numberOfTimes; i++) {
+      const word = getRandomWordSync({ withErrors: false });
+      console.log( `${i}: ${(i%3 ? '':'Fizz')+(i%5 ? '':'Buzz') || word}`)
+    }
+  } else {
+    console.log('Positive number input is required')
+  }
+};
+
+
+//printNumbersWithRandomWords(100);
+
+printNumbersWordsAndFizzBuzz(100);
