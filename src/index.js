1c1fcb2411a2568e68d5a8b3691db68dae79d00d
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-10-01 14:59:07 +0530

    completion up to logger integration

--- src/index.js
+++ src/index.js
@@ -25,8 +25,14 @@ const printNumbersWithRandomWords = (numberOfTimes) => {
 const printNumbersWordsAndFizzBuzz = (numberOfTimes) => {
   if(!isNaN(numberOfTimes) && numberOfTimes > 0){
     for(let i=1; i <= numberOfTimes; i++) {
-      const word = getRandomWordSync({ withErrors: false });
-      console.log( `${i}: ${(i%3 ? '':'Fizz')+(i%5 ? '':'Buzz') || word}`);
+      let word, errorMessage = null;
+      try{
+        word = getRandomWordSync({ withErrors: true });
+      } catch (e) {
+        errorMessage = "It shouldn't break anything!";
+      }
+      const nonErrorPrintable = `${(i%3 ? '':'Fizz')+(i%5 ? '':'Buzz') || word}`;
+        console.log( `${i}: ${(errorMessage != null)? errorMessage : nonErrorPrintable}`);
     }
   } else {
     console.log('Positive number input is required');
@@ -53,7 +59,7 @@ const getFormattedStringWithNumbers = async(numberOfTimes, i) => {
  * @param i - index
  * @returns {Promise.<void>}
  */
-const getFormattedStringWithNumbersAndFizzBizz = async(numberOfTimes, i) => {
+const getFormattedStringWithNumbersAndFizzBuzz = async(numberOfTimes, i) => {
   let word, errorMessage=null;
   try{
     word = await getRandomWord({ withErrors : true, slow : true });
@@ -63,15 +69,28 @@ const getFormattedStringWithNumbersAndFizzBizz = async(numberOfTimes, i) => {
   if(numberOfTimes > 0){
     const nonErrorPrintable = `${(i%3 ? '':'Fizz')+(i%5 ? '':'Buzz') || word}`;
     console.log(`${i}: ${(errorMessage != null)? errorMessage : nonErrorPrintable}`);
-    return await getFormattedStringWithNumbersAndFizzBizz(numberOfTimes - 1, i + 1);
+    return await getFormattedStringWithNumbersAndFizzBuzz(numberOfTimes - 1, i + 1);
   }
 };
 
 
+/**
+ * uncomment to see first task
+ */
 //printNumbersWithRandomWords(100);
 
-//printNumbersWordsAndFizzBuzz(100);
+/**
+ * second task and modifications with 4th task, error handling
+ */
+printNumbersWordsAndFizzBuzz(100);
 
+/**
+ * first task with asynchronous getRandomWord with support of recursion
+ */
 //getFormattedStringWithNumbers(100, 1);
 
-//getFormattedStringWithNumbersAndFizzBizz(100, 1);
+/**
+ * second task with asynchronous getRandomWord with support of recursion
+ * modified to handle errors
+ */
+getFormattedStringWithNumbersAndFizzBuzz(100, 1);
