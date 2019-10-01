b982fdff33a8e8033d2b2dd2a4ddc60f069b7a85
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-10-01 14:35:04 +0530

    adding async error handling

--- src/index.js
+++ src/index.js
@@ -33,7 +33,45 @@ const printNumbersWordsAndFizzBuzz = (numberOfTimes) => {
   }
 };
 
+/**
+ * @description prints 1 to `numberOfTimes` with use of getRandomWord
+ * @param numberOfTimes
+ * @param i - index
+ * @returns {Promise.<void>}
+ */
+const getFormattedStringWithNumbers = async(numberOfTimes, i) => {
+  const word = await getRandomWord({ withErrors : false, slow : true });
+  if(numberOfTimes > 0){
+    console.log(`${i}: ${word}`);
+    return await getFormattedStringWithNumbers(numberOfTimes - 1, i +1);
+  }
+};
+
+/**
+ * @description prints 1 to `numberOfTimes` with use of getRandomWord with FizzBizz
+ * @param numberOfTimes
+ * @param i - index
+ * @returns {Promise.<void>}
+ */
+const getFormattedStringWithNumbersAndFizzBizz = async(numberOfTimes, i) => {
+  let word, errorMessage=null;
+  try{
+    word = await getRandomWord({ withErrors : true, slow : true });
+  } catch (e) {
+    errorMessage = "It shouldn't break anything!";
+  }
+  if(numberOfTimes > 0){
+    const nonErrorPrintable = `${(i%3 ? '':'Fizz')+(i%5 ? '':'Buzz') || word}`;
+    console.log(`${i}: ${(errorMessage != null)? errorMessage : nonErrorPrintable}`);
+    return await getFormattedStringWithNumbersAndFizzBizz(numberOfTimes - 1, i + 1);
+  }
+};
+
 
 //printNumbersWithRandomWords(100);
 
-printNumbersWordsAndFizzBuzz(100);
+//printNumbersWordsAndFizzBuzz(100);
+
+//getFormattedStringWithNumbers(100, 1);
+
+//getFormattedStringWithNumbersAndFizzBizz(100, 1);
