4f65ee947f9fd07d719dcc94107752e8b67ddcfe
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-10-01 12:57:32 +0530

    task one completion

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+const printNumbersWithRandomWords = (numberOfTimes) => {
+  if(!isNaN(numberOfTimes) && numberOfTimes > 0){
+    for(var i=0; i <= numberOfTimes; i++) {
+      let word = getRandomWordSync({ withErrors: false });
+      console.log(`${i}: ${word}`)
+    }
+  } else {
+    console.log('Positive number input is required')
+  }
+};
+
+printNumbersWithRandomWords(100);
